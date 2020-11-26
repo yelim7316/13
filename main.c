09d95248c3a5a7797c9daa5dabb8f9380d019b09
yelim7316 <70632863+yelim7316@users.noreply.github.com>
2020-11-26 15:20:50 +0900

    01

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct student
+{
+	int ID;
+	char name[100];
+	double grade;
+	
+};
+
+int main(int argc, char *argv[]) {
+	
+	struct student s1 = {123456, "yerim", 4.3};
+	
+	s1.ID = 2015660;
+	s1.grade = 4.2;
+	
+
+	printf("ID : %d\n", s1.ID);
+	printf("name : %s\n", s1.name);
+	printf("grade : %f\n", s1.grade);
+	
+	return 0;
+}
